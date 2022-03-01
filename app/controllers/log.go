f09c2a3deb36aebb0e617c171b615c14bba0cdab
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-03-01 22:02:25 +0900

    :+1: Add controllers

--- app/controllers/log.go
+++ app/controllers/log.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"ushas/models"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,4 +19,103 @@ func (lc *LogController) Index(c echo.Context) error {
 		http.StatusText(http.StatusOK),
 		"OK",
 	))
+}
+
+// SerpViewingLogRequest : Struct for task viewing time log request body
+type SerpViewingLogRequest struct {
+	// UserID : The ID of user (worker)
+	UserID int `json:"user"`
+
+	// TaskID : The ID of task that user working.
+	TaskID int `json:"task"`
+
+	// ConditionId : User's condition ID that means group and task category.
+	ConditionId int `json:"condition"`
+}
+
+func (lc *LogController) CreateSerpViewingLog(c echo.Context) error {
+	if lc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	p := SerpViewingLogRequest{}
+	if err := c.Bind(&p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, "Cannot bind request body")
+	}
+
+	if err := c.Validate(&p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, p)
+	}
+
+	log := &models.SerpViewingLog{
+		UserID: p.UserID,
+		TaskID: p.TaskID,
+		ConditionID: p.ConditionId,
+	}
+	if err := models.CreateSerpViewingLog(log); err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, p)
+	}
+
+	return c.JSON(http.StatusCreated, newResponse(
+		http.StatusCreated,
+		http.StatusText(http.StatusCreated),
+		p,
+	))
+}
+
+// PageViewingLogRequest : Struct for each search result page viewing time log request body
+type PageViewingLogRequest struct {
+	// UserID : The ID of user (worker)
+	UserID int `json:"user"`
+
+	// TaskID : The ID of task that user working.
+	TaskID int `json:"task"`
+
+	// ConditionID : User's condition ID that means group and task category.
+	ConditionID int `json:"condition"`
+
+	// PageID : Page ID that user view.
+	PageID int `json:"page"`
+}
+
+// SearchPageEventLogRequest : Struct for page click log request body.
+type SearchPageEventLogRequest struct {
+	// ID : The ID of each log record.
+	ID string `json:"id"`
+
+	// Uid : The ID of user (worker)
+	User int `json:"user"`
+
+	// TaskID : The ID of task that user working.
+	TaskID int `json:"task"`
+
+	// ConditionID : User's condition ID that means group and task category.
+	ConditionID int `json:"condition"`
+
+	// Time : User's page viewing time.
+	Time int `json:"time"`
+
+	// Page : The ID of page that user clicked.
+	Page int `json:"page"`
+
+	// Rank : Search result rank that user clicked.
+	Rank int `json:"rank"`
+
+	// IsVisible : Risk is visible or not.
+	IsVisible bool `json:"visible"`
+
+	// Event : It is expected to be "click", "hover" or "paginate"
+	Event string `json:"event"`
+}
+
+// SearchSessionRequest : Struct fot search session request body.
+type SearchSessionRequest struct {
+	// UserID : Assigned ID of user (worker)
+	UserID int `json:"user"`
+
+	// TaskID : The ID of task that user working.
+	TaskID int `json:"task"`
+
+	// ConditionID : User's condition ID that means group and task category.
+	ConditionID int `json:"condition"`
 }
--- app/controllers/task.go
+++ app/controllers/task.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
+	"ushas/models"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,5 +19,173 @@ func (tc *TaskController) Index(c echo.Context) error {
 		http.StatusOK,
 		http.StatusText(http.StatusOK),
 		"OK",
+	))
+}
+
+type CreateTaskRequest struct {
+	// Query : Search query for this task.
+	Query string `json:"query"`
+
+	// Title : Title of this task.
+	Title string `json:"title"`
+
+	// Description : Description text of task.
+	Description string `json:"description"`
+
+	// SearchUrl : Url used in this task.
+	SearchUrl string `json:"searchUrl"`
+}
+
+func (tc *TaskController) Create(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	p := CreateTaskRequest{}
+	if err := c.Bind(&p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, nil)
+	}
+
+	if err := c.Validate(p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, p)
+	}
+
+	t := &models.Task{
+		Query:       p.Query,
+		Title:       p.Title,
+		Description: p.Description,
+		SearchUrl:   p.SearchUrl,
+	}
+	if err := models.CreateTask(t); err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, nil)
+	}
+
+	return c.JSON(http.StatusCreated, newResponse(
+		http.StatusCreated,
+		http.StatusText(http.StatusCreated),
+		t,
+	))
+}
+
+func (tc *TaskController) AllocateTask(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		"",
+	))
+}
+
+func (tc *TaskController) GetTaskByID(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	p := c.Param("id")
+	if len(p) > 0 {
+		return newErrResponse(c, http.StatusBadRequest, nil, p)
+	}
+
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, id)
+	}
+
+	t, err := models.GetTaskByID(id)
+	if err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, p)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(http.StatusOK, http.StatusText(http.StatusOK), t))
+}
+
+func (tc *TaskController) ListTask(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	tasks, err := models.ListTasks()
+	if err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, tasks)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		tasks,
+	))
+}
+
+type UpdateTaskRequest struct {
+	// ID : The ID of task
+	ID int `db:"id" json:"id"`
+
+	// Query : Search query for this task.
+	Query string `db:"query" json:"query"`
+
+	// Title : Title of this task.
+	Title string `db:"title" json:"title"`
+
+	// Description : Description text of task.
+	Description string `db:"description" json:"description"`
+
+	// SearchUrl : Url used in this task.
+	SearchUrl string `db:"search_url" json:"searchUrl"`
+}
+
+func (tc *TaskController) UpdateTask(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	p := UpdateTaskRequest{}
+	if err := c.Bind(&p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, p)
+	}
+
+	if err := c.Validate(p); err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, p)
+	}
+
+	t := &models.Task{
+		ID:          p.ID,
+		Query:       p.Query,
+		Title:       p.Title,
+		Description: p.Description,
+		SearchUrl:   p.SearchUrl,
+	}
+	if err := models.UpdateTask(t); err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, t)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		t,
+	))
+}
+
+func (tc *TaskController) DeleteTask(c echo.Context) error {
+	if tc == nil {
+		return newErrResponse(c, http.StatusInternalServerError, nil, nil)
+	}
+
+	p := c.Param("id")
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		return newErrResponse(c, http.StatusBadRequest, err, "Task ID must be number")
+	}
+
+	if err := models.DeleteTask(id); err != nil {
+		return newErrResponse(c, http.StatusInternalServerError, err, id)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		"",
 	))
 }
